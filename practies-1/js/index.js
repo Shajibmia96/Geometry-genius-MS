ce67ca0a6fd314670caf59c2e6da9394fb4c1084
Shajib96 <shajibahamedornob@gmail.com>
2023-11-02 12:10:18 +0600

    Tranagle && Rectangle section done

---
+++ practies-1/js/index.js
@@ -0,0 +1,13 @@
+function getRectangleCalculate(){
+    // console.log("hello ")
+    const getWidthElement = document.getElementById("rectangle-width");
+     const getWidth = parseFloat(getWidthElement.value);
+     getWidthElement.value = '';
+    //  console.log(getWidth)
+    const getLengthElement = document.getElementById("rectangle-length");
+    const getLength = parseFloat(getLengthElement.value);  
+    getLengthElement.value = '';
+    const rectangleTotal  = getWidth * getLength
+    const rectangle =document.getElementById("Rectangle-total");
+     rectangle.innerText = rectangleTotal; 
+}
--- practies-1/js/utility.js
+++ practies-1/js/utility.js
@@ -1,3 +1,25 @@
  function getTriangleCalculate(){
-    // console.log('find it')
+   //  console.log('find it')
+    const getInputField =document.getElementById("triangle-base")
+    const getInputValue = getInputField.value;
+    getInputField.value = '';
+    const inputValueBase  = parseFloat(getInputValue);
+
+    const getInputFieldValue =document.getElementById("triangle-height")
+    const getInputValueHeight = getInputFieldValue.value;
+    getInputFieldValue.value = '';
+    const inputValueHeight  = parseFloat(getInputValueHeight);
+   //  return inputValue;
+   //  console.log(inputValue)
+   const aria = 1/2* inputValueBase * inputValueHeight;
+   console.log(aria);
+
+   const getAriaTotal = document.getElementById("AriaTotal")
+   // console.log(getAriaTotal)
+    getAriaTotal.innerText = aria;
  }
+//   const TotalAria = inputValue;
+//   console.log(TotalAria)
+//  getTriangleCalculate("triangle-base")
+// //  getTriangleCalculate("triangle-height")
+
